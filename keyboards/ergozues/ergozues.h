81bfcf5038aea97cffcf13f0ab1a4e5ae9f1976f
Zuhayr Chagpar <zuhayrchagpar@gmail.com>
2020-07-25 11:54:52 +0100

    Reorder layout
    Now it works!

--- keyboards/ergozues/ergozues.h
+++ keyboards/ergozues/ergozues.h
@@ -27,23 +27,14 @@
  * represents the switch matrix.
  */
 #define KEYMAP( \
-    K00, K01, K02, K03, K04, K05, K06, \
-    K10, K11, K12, K13, K14, K15, K16, \
-    K20, K21, K22, K23, K24, K25,      \
-    K30, K31, K32, K33, K34, K35, K36, \
-    K40, K41, K42, K43, K44,           \
-                             K55, K56, \
-                                  K54, \
-                        K53, K52, K51, \
-                                       \
-    K07, K08, K09, K0A, K0B, K0C, K0D, \
-    K17, K18, K19, K1A, K1B, K1C, K1D, \
-         K28, K29, K2A, K2B, K2C, K2D, \
-    K37, K38, K39, K3A, K3B, K3C, K3D, \
-              K49, K4A, K4B, K4C, K4D, \
-    K57, K58,                          \
-    K59,                               \
-    K5C, K5B, K5A                      \
+    K00, K01, K02, K03, K04, K05, K06,     K07, K08, K09, K0A, K0B, K0C, K0D, \
+    K10, K11, K12, K13, K14, K15, K16,     K17, K18, K19, K1A, K1B, K1C, K1D, \
+    K20, K21, K22, K23, K24, K25,               K28, K29, K2A, K2B, K2C, K2D, \
+    K30, K31, K32, K33, K34, K35, K36,     K37, K38, K39, K3A, K3B, K3C, K3D, \
+    K40, K41, K42, K43, K44,                         K49, K4A, K4B, K4C, K4D, \
+                             K55, K56,     K57, K58,                          \
+                                  K54,     K59,                               \
+                        K53, K52, K51,     K5C, K5B, K5A                      \
 )    {                                                            \
     { K00, K10, K20, K30, K40, KC_NO },   \
     { K01, K11, K21, K31, K41, K51 },   \
@@ -53,11 +44,11 @@
     { K05, K15, K25, K35, KC_NO, K55 },   \
     { K06, K16, KC_NO, K36, KC_NO, K56 },   \
                                                                  \
-    { K07, K17, KC_NO, K37,KC_NO, K57 },   \
-    { K08, K18, K28, K38,KC_NO, K58 },   \
-    { K09, K19, K29, K39, K49, K59 },   \
-    { K0A, K1A, K2A, K3A, K4A, K5A },   \
-    { K0B, K1B, K2B, K3B, K4B, K5B },   \
+    { K0D, K1D, K2D, K3D, K4D, KC_NO },    \
     { K0C, K1C, K2C, K3C, K4C, K5C },   \
-    { K0D, K1D, K2D, K3D, K4D, KC_NO }    \
-   }
+    { K0B, K1B, K2B, K3B, K4B, K5B },   \
+    { K0A, K1A, K2A, K3A, K4A, K5A },   \
+    { K09, K19, K29, K39, K49, K59 },   \
+    { K08, K18, K28, K38,KC_NO, K58 },   \
+    { K07, K17, KC_NO, K37,KC_NO, K57 }   \
+}
--- keyboards/ergozues/keymaps/default/keymap.c
+++ keyboards/ergozues/keymaps/default/keymap.c
@@ -38,12 +38,14 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KEYMAP(
-      KC_0, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_A, KC_B, KC_C, KC_D,
-      KC_0, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_A, KC_B, KC_C, KC_D,
-      KC_0, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_A, KC_B,
-      KC_0, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_A, KC_B, KC_C, KC_D,
-      KC_0, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, 
-      KC_0, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_A, KC_B
+      KC_0,       KC_1,       KC_2,     KC_3,       KC_4,      KC_5,      KC_6,           KC_7,       KC_8,       KC_9,       KC_A,     KC_B,       KC_C,      KC_D,
+      KC_0,       KC_1,       KC_2,     KC_3,       KC_4,      KC_5,      KC_6,           KC_7,       KC_8,       KC_9,       KC_A,     KC_B,       KC_C,      KC_D,
+      KC_0,       KC_1,       KC_2,     KC_3,       KC_4,      KC_5,                                  KC_6,       KC_7,       KC_8,     KC_9,       KC_A,      KC_B,
+      KC_0,       KC_1,       KC_2,     KC_3,       KC_4,      KC_5,      KC_6,           KC_7,       KC_8,       KC_9,       KC_A,     KC_B,       KC_C,      KC_D,
+      KC_0,       KC_1,       KC_2,     KC_3,       KC_4,                                                         KC_5,       KC_6,     KC_7,       KC_8,      KC_9,
+                                                              KC_0,       KC_1,           KC_2,       KC_3,
+                                                                          KC_4,           KC_5,
+                                                  KC_6,       KC_7,       KC_8,           KC_9,       KC_A,     KC_B
     )
 };
 
