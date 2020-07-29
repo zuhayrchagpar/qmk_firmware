d71e8f278dda73ee1cb65da04714cb4c5d8f29a3
Zuhayr Chagpar <zuhayrchagpar@gmail.com>
2020-07-29 21:47:17 +0100

    Change Symbbb numbers to use keypad

--- keyboards/ergozues/keymaps/default/keymap.c
+++ keyboards/ergozues/keymaps/default/keymap.c
@@ -51,11 +51,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
   [SYMB] = KEYMAP(
-    KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_TRNS,           KC_TRNS, KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
-    KC_TRNS,  KC_EXLM,  KC_AT,    KC_LCBR,  KC_RCBR,  KC_PIPE,  KC_TRNS,           KC_TRNS, KC_UP,    KC_7,     KC_8,     KC_9,     KC_ASTR,  KC_F12,
-    KC_TRNS,  KC_HASH,  KC_DLR,   KC_LPRN,  KC_RPRN,  KC_GRV,                               KC_DOWN,  KC_4,     KC_5,     KC_6,     KC_PLUS,  KC_PAUS,
-    KC_TRNS,  KC_PERC,  KC_CIRC,  KC_LBRC,  KC_RBRC,  KC_TILD,  KC_TRNS,           KC_TRNS, KC_AMPR,  KC_1,     KC_2,     KC_3,     KC_BSLS,  KC_SLCK,
-    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                                                  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_TRNS,           KC_TRNS, KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,     KC_F11,
+    KC_TRNS,  KC_EXLM,  KC_AT,    KC_LCBR,  KC_RCBR,  KC_PIPE,  KC_TRNS,           KC_TRNS, KC_UP,    KC_KP_7,  KC_KP_8,  KC_KP_9,  KC_ASTR,    KC_F12,
+    KC_TRNS,  KC_HASH,  KC_DLR,   KC_LPRN,  KC_RPRN,  KC_GRV,                               KC_DOWN,  KC_KP_4,  KC_KP_5,  KC_KP_6,  KC_PLUS,    KC_PAUS,
+    KC_TRNS,  KC_PERC,  KC_CIRC,  KC_LBRC,  KC_RBRC,  KC_TILD,  KC_TRNS,           KC_TRNS, KC_AMPR,  KC_KP_1,  KC_KP_2,  KC_KP_3,  KC_BSLS,    KC_SLCK,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                                                  KC_TRNS,  KC_TRNS,  KC_KP_0,  KC_KP_DOT,  KC_TRNS,
                                                       KC_TRNS,  KC_TRNS,           KC_TRNS, KC_TRNS,
                                                                 KC_TRNS,           KC_TRNS,
                                             KC_TRNS,  KC_TRNS,  KC_TRNS,           KC_TRNS, KC_TRNS,KC_TRNS
